bfe525d4b9b328a9d09aca2f1d3245be5b149634
Manuel Cruz <cruzmanuelar@gmail.com>
2022-08-19 00:33:48 -0500

    Inicializacioan

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, model_random_forest, model_clustering, model_svm, model_regresion_logistica, model_lib_prophet # import your app modules here
+# from apps import home, model_random_forest, model_clustering, model_svm, model_regresion_logistica, model_lib_prophet # import your app modules here
+
+from apps import home
 
 app = MultiApp()
 
@@ -10,10 +12,10 @@ st.markdown("""
 
 # Add all your application here
 app.add_app("Home", home.app)
-app.add_app("Modelo Random Forest", model_random_forest.app)
-app.add_app("Modelo Regresión Logística", model_regresion_logistica.app)
-app.add_app("Modelo SVM de Regresión", model_svm.app)
-app.add_app("Modelo basado en la librería Prophet", model_lib_prophet.app)
-app.add_app("Caso de asociación clustering", model_clustering.app)
+# app.add_app("Modelo Random Forest", model_random_forest.app)
+# app.add_app("Modelo Regresión Logística", model_regresion_logistica.app)
+# app.add_app("Modelo SVM de Regresión", model_svm.app)
+# app.add_app("Modelo basado en la librería Prophet", model_lib_prophet.app)
+# app.add_app("Caso de asociación clustering", model_clustering.app)
 # The main app
 app.run()
