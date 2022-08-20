fa6da6a8b29721aa2a49f7ca2145d0628b4509cf
Manuel Cruz <cruzmanuelar@gmail.com>
2022-08-19 22:40:41 -0500

    ari

--- apps/model_arima.py
+++ apps/model_arima.py
@@ -19,7 +19,7 @@ def app():
 
     
     scaler = MinMaxScaler(feature_range=(0, 1))
-    st.title('Modelo - Arima')
+    st.title('Modelo - Arima 1 ')
     ticker='MSFT'
     period1 = int(time.mktime(datetime.datetime(2015, 1, 1, 0, 0).timetuple()))
     period2 = int(time.mktime(datetime.datetime.now().timetuple()))
@@ -27,6 +27,7 @@ def app():
     query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
     df_dis = pd.read_csv(query_string)
 
+    st.title('Modelo - Arima 2 ')
     data = df_dis.sort_index(ascending=True, axis=0)
 
     train = data[:1551]
@@ -34,10 +35,11 @@ def app():
 
     training = train['Close']
     validation = valid['Close']
-
+    st.title('Modelo - Arima 3 ')
     model = auto_arima(training, start_p=1, start_q=1,max_p=3, max_q=3, m=12,start_P=0, seasonal=True,d=1, D=1, trace=True,error_action='ignore',suppress_warnings=True)
     model.fit(training)
 
+    st.title('Modelo - Arima 4 ')
     forecast = model.predict(n_periods=351)
     forecast = pd.DataFrame(forecast,index = valid.index,columns=['Prediction'])
 
