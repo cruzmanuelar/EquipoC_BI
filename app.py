235b29fce6a647660ff656b0720695d7c75d782f
Manuel Cruz <cruzmanuelar@gmail.com>
2022-08-19 22:15:03 -0500

    arimaa

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from multiapp import MultiApp
 # from apps import home, model_random_forest, model_clustering, model_svm, model_regresion_logistica, model_lib_prophet # import your app modules here
 
+from apps import home, model_arima # import your app modules here
+
 from apps import home
 
 app = MultiApp()
@@ -13,6 +15,7 @@ st.markdown("""
 # Add all your application here
 app.add_app("Home", home.app)
 # app.add_app("Modelo Random Forest", model_random_forest.app)
+app.add_app("Modelo Arima", model_arima.app)
 # app.add_app("Modelo Regresión Logística", model_regresion_logistica.app)
 # app.add_app("Modelo SVM de Regresión", model_svm.app)
 # app.add_app("Modelo basado en la librería Prophet", model_lib_prophet.app)
