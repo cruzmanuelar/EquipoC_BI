5f1b79f959b9006b89acde1d8c7ff43bb3cb3cb0
Manuel Cruz <cruzmanuelar@gmail.com>
2022-08-24 21:20:24 -0500

    bitcoin head

--- arima.py
+++ arima.py
@@ -3,3 +3,8 @@ import pandas as pd
 from statsmodels.tsa.arima_model import ARIMA
 
 st.write('Prueba')
+
+bitcoin = pd.read_csv("bitcoin_price.csv",parse_dates=['Date'])
+bitcoin = bitcoin.set_index('Date')
+bitcoin.sort_index(inplace=True)
+bitcoin.head()
