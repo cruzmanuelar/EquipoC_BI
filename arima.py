ccf2cec1858832eda73f0924584f25eb3a59b4a9
Manuel Cruz <cruzmanuelar@gmail.com>
2022-08-24 21:38:32 -0500

    bitcoin

--- arima.py
+++ arima.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from statsmodels.tsa.arima_model import ARIMA
 import yfinance as yf
+import matplotlib.pyplot as plt 
 
 st.title('Learning Data Science — Predict Stock Price with Support Vector Regression (SVR)')
 start = '2019-01-01'
@@ -16,3 +17,6 @@ df['Date'] = pd.to_datetime(df.Date, format='%Y-%m-%d')
 df.index = df['Date']
 
 st.write(df)
+
+plt.figure(figsize=(16,8))
+plt.plot(df['Close'], label='Precio de cierre MSFT')
