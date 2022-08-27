35009dd0378c40324ff39708723453559636e824
Manuel Cruz <cruzmanuelar@gmail.com>
2022-08-27 09:14:38 -0500

    arima

--- arima.py
+++ arima.py
@@ -3,81 +3,96 @@ import pandas as pd
 from statsmodels.tsa.arima_model import ARIMA
 import yfinance as yf
 import matplotlib.pyplot as plt
-from pmdarima.arima import auto_arima
 import numpy as np
+import time
+import datetime
 
-st.title('Learning Data Science — Predict Stock Price with Support Vector Regression (SVR)')
-start = '2015-01-02'
-end = '2022-08-24'
-st.subheader('Preparación de la data')
 
-df_msft = yf.download('MSFT', start, end)
-df_msft = df_msft.reset_index()
-st.write(df_msft)
-st.write(df_msft.shape)
+st.title('Modelo ARIMA')
 
-df_msft['Date'] = pd.to_datetime(df_msft.Date, format='%Y-%m-%d')
-df_msft.index = df_msft['Date']
+ticker='AMZN'
+period1 = int(time.mktime(datetime.datetime(2015, 1, 1, 0, 0).timetuple()))
+period2 = int(time.mktime(datetime.datetime.now().timetuple()))
+interval = '1d' # 1d, 1m
+query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
+df_amzn = pd.read_csv(query_string)
 
-st.write(df_msft)
+df_amzn['Date'] = pd.to_datetime(df_amzn.Date, format='%Y-%m-%d')
+df_amzn.index = df_amzn['Date']
 
-fig = plt.figure(figsize=(16,8))
-plt.plot(df_msft['Close'], label='Precio de cierre MSFT')
-# plt.show()
-st.pyplot(fig)
+fig = plt.figure(figsize=(14,8))
+plt.plot(df_amzn['Close'], label='Precio de cierre AMZN')
 
-data = df_msft.sort_index(ascending=True, axis = 0)
-st.write(data)
+# df_msft = yf.download('MSFT', start, end)
+# df_msft = df_msft.reset_index()
+# st.write(df_msft)
+# st.write(df_msft.shape)
 
-nueva_data = pd.DataFrame(index=range(0, len(df_msft)), columns=['Date','Close'])
+# st.pyplot(fig)
+# st.subheader('Preparación de la data')
 
-for i in range(0, len(data)):
-  nueva_data['Date'][i] = data['Date'][i]
-  nueva_data['Close'][i] = data['Close'][i]
+# df_msft['Date'] = pd.to_datetime(df_msft.Date, format='%Y-%m-%d')
+# df_msft.index = df_msft['Date']
 
-st.write(nueva_data.head())
+# st.write(df_msft)
 
-train = nueva_data[:1348]
-valid = nueva_data[1348:]
-st.write(nueva_data.shape)
-st.write(train.shape)
-st.write(valid.shape)
+# fig = plt.figure(figsize=(16,8))
+# plt.plot(df_msft['Close'], label='Precio de cierre MSFT')
+# # plt.show()
+# st.pyplot(fig)
 
-data = df_msft.sort_index(ascending=True, axis=0)
+# data = df_msft.sort_index(ascending=True, axis = 0)
+# st.write(data)
 
-train = data[:1348]
-valid = data[1341:]
+# nueva_data = pd.DataFrame(index=range(0, len(df_msft)), columns=['Date','Close'])
 
-training = train['Close']
-validation = valid['Close']
+# for i in range(0, len(data)):
+#   nueva_data['Date'][i] = data['Date'][i]
+#   nueva_data['Close'][i] = data['Close'][i]
 
-model = auto_arima(training, start_p=1, start_q=1,max_p=3, suppress_warning=True, max_q=3, m=12, start_P=0, seasonal=True, d=1, D=1, trace= True, error_action='ignore')
-model.fit(training)
+# st.write(nueva_data.head())
 
-forecast = model.predict(n_periods=576)
-forecast = pd.DataFrame(forecast,index = valid.index,columns=['Prediction'])
+# train = nueva_data[:1348]
+# valid = nueva_data[1348:]
+# st.write(nueva_data.shape)
+# st.write(train.shape)
+# st.write(valid.shape)
 
-rms=np.sqrt(np.mean(np.power((np.array(valid['Close'])-np.array(forecast['Prediction'])),2)))
-st.write(rms)
+# data = df_msft.sort_index(ascending=True, axis=0)
 
-st.subheader('Preparación de la data')
+# train = data[:1348]
+# valid = data[1341:]
 
-#plot
-plt.plot(train['Close'])
-plt.plot(valid['Close'])
-plt.plot(forecast['Prediction'])
+# training = train['Close']
+# validation = valid['Close']
 
-fig1 = plt.figure(figsize=(16,8))
-plt.plot(train['Close'], label='Precio de cierre MSFT')
-# plt.show()
-st.pyplot(fig1)
+# model = auto_arima(training, start_p=1, start_q=1,max_p=3, suppress_warning=True, max_q=3, m=12, start_P=0, seasonal=True, d=1, D=1, trace= True, error_action='ignore')
+# model.fit(training)
 
-fig2 = plt.figure(figsize=(16,8))
-plt.plot(valid['Close'], label='Precio de cierre MSFT')
-# plt.show()
-st.pyplot(fig2)
+# forecast = model.predict(n_periods=576)
+# forecast = pd.DataFrame(forecast,index = valid.index,columns=['Prediction'])
 
-fig3 = plt.figure(figsize=(16,8))
-plt.plot(forecast['Close'], label='Precio de cierre MSFT')
-# plt.show()
-st.pyplot(fig3)
+# rms=np.sqrt(np.mean(np.power((np.array(valid['Close'])-np.array(forecast['Prediction'])),2)))
+# st.write(rms)
+
+# st.subheader('Preparación de la data')
+
+# #plot
+# plt.plot(train['Close'])
+# plt.plot(valid['Close'])
+# plt.plot(forecast['Prediction'])
+
+# fig1 = plt.figure(figsize=(16,8))
+# plt.plot(train['Close'], label='Precio de cierre MSFT')
+# # plt.show()
+# st.pyplot(fig1)
+
+# fig2 = plt.figure(figsize=(16,8))
+# plt.plot(valid['Close'], label='Precio de cierre MSFT')
+# # plt.show()
+# st.pyplot(fig2)
+
+# fig3 = plt.figure(figsize=(16,8))
+# plt.plot(forecast['Close'], label='Precio de cierre MSFT')
+# # plt.show()
+# st.pyplot(fig3)
